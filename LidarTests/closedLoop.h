897948d7824bd23a8b6ee3ff8ca237eba6fa263c
ChrisChampagne <cchamp@seas.upenn.edu>
2015-04-30 05:24:50 -0400

    added ifndef for position and closedLoop struct and function headerfile

---
+++ LidarTests/closedLoop.h
@@ -0,0 +1,37 @@
+/*
+ *  closedLoop.h
+ *  Header file for movement
+ */
+
+  //could also use this for making enum a type:
+ //add whatever behaviors need to be kept track of or added
+typedef enum {STOPSIGN, DESTINATION, INTERSECTION, NONE} behavior;  //(behavior is the type)
+
+#ifndef POSITION
+typedef struct position {
+  double x;
+  double y;
+  double direction; //in degrees
+} position;
+#endif
+
+typedef struct linkednode
+{ 
+  behavior behav;
+  LINKEDNODE* next;
+} LINKEDNODE;
+
+//made coordinates doubles because thats what they are in position struct already
+typedef struct node
+{ 
+  int cost;
+  int direction;
+  double xCoord;
+  double yCoord;
+  double speedLimit;
+  LINKEDNODE* neighbors;
+} NODE;
+
+void closedLoopControlToNextPoint(position currPos, LINKEDNODE nextNode);
+
+LINKEDNODE* planRoute(position currPos, float destX, float destY);
--- LidarTests/lidarProcessor.h
+++ LidarTests/lidarProcessor.h
@@ -25,11 +25,13 @@ typedef struct cartesian_line {
   double m;
 } cartesian_line;
 
+#ifndef POSITION
 typedef struct position {
   double x;
   double y;
   double direction; //in degrees
 } position;
+#endif
 
 unsigned short int* houghTransform(REVOLUTION_DATA* lidar_data);
 
